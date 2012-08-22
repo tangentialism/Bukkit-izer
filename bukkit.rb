20c302f43b014116df6c50ce2df24d98b2219928
David Yee <tangentialism@gmail.com>
2012-08-22 11:09:04 -0400

    allow to find a file of format

--- bukkit.rb
+++ bukkit.rb
@@ -4,7 +4,20 @@ require 'open-uri'
 require 'json'
 
 get '/' do
-  doc = Nokogiri::HTML(open('http://bukk.it'))
-  bukkits = doc.css('tr > td > a').collect(&:content)
+  bukkits = get_bukkits()
   redirect "http://bukk.it/#{bukkits.sample}" 
 end
+
+get '/*.:format' do
+  bukkits = get_bukkits()
+  pretties = bukkits.select do |b|
+    /\.#{params[:format]}/.match b
+  end
+  redirect "http://bukk.it/#{pretties.sample}" 
+end
+
+def get_bukkits
+  doc = Nokogiri::HTML(open('http://bukk.it'))
+  bukkits = doc.css('tr > td > a').collect(&:content)
+end
+
