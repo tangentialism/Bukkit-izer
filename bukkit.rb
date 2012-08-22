d6ce40660df8cc8cf6219818f44c736b299a4580
David Yee <tangentialism@gmail.com>
2012-08-22 12:01:35 -0400

    look for strings inside the request

--- bukkit.rb
+++ bukkit.rb
@@ -8,10 +8,17 @@ get '/' do
   redirect "http://bukk.it/#{bukkits.sample}" 
 end
 
-get '/*.:format' do
+get '/favicon.ico' do
+  return 404
+end
+
+get '/:matchword.:format' do
   bukkits = get_bukkits()
   pretties = bukkits.select do |b|
-    /\.#{params[:format]}/.match b
+    match_word_and_format(params[:matchword], params[:format], b)
+  end
+  if pretties.length < 1
+    return 404
   end
   redirect "http://bukk.it/#{pretties.sample}" 
 end
@@ -21,3 +28,16 @@ def get_bukkits
   bukkits = doc.css('tr > td > a').collect(&:content)
 end
 
+def match_word_and_format(word, format, b)
+  if format
+    if word == 'index'
+      return b.match ".#{format}"
+    else
+      return b.match(".#{format}") && b.match("#{word}")
+    end
+  else
+    unless word == 'index'
+      return b.match "#{word}"
+    end
+  end
+end
